621786706314c54a4e2c5f76f328235111393df2
Binny K Thomas <binny.thomas@axelerant.com>
2020-03-31 18:38:12 +0530

    Issue #3123743 by binnythomas: Create a route and controller that sets the tracking cookie

---
+++ src/Controller/SetTracking.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\simple_affiliate\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class SetTracking.
+ *
+ * @package Drupal\simple_affiliate\Controller
+ */
+class SetTracking extends ControllerBase {
+
+  /**
+   * Setting the tracking cookie.
+   */
+  public function setaffilliatetrackingcookie($uid = 10) {
+    // Add affiliate cookie with 6 month expiration.
+    if (!isset($_COOKIE["simple_affiliate"])) {
+      setcookie("simple_affiliate", $uid, time() + 60 * 60 * 24 * 180, "/");
+    }
+    return $this->redirect('<front>');
+
+  }
+
+}

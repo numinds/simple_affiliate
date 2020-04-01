233ae6b42ff528bae4ea2999b893036764a72eb3
binny_zyxware <binny_zyxware@3123091.no-reply.drupal.org>
2020-04-01 13:54:55 +0530

    Issue #3123838 by binnythomas: Create a block which displays the affiiliate link for the registered user.

---
+++ src/Plugin/Block/AffiliateLinkBlock.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Drupal\simple_affiliate\Plugin\Block;
+
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Provides automatically generated affiliate link with user id.
+ *
+ * @Block(
+ *   id = "simple_affiliate_referral_link",
+ *   admin_label = @Translation("Simple Affiliate Referral Link Block"),
+ * )
+ */
+class AffiliateLinkBlock extends BlockBase {
+
+  /**
+   * @inheritDoc
+   */
+  public function build() {
+
+    // Getting the current used id.
+    $user_id = \Drupal::currentUser()->id();
+
+    // Generating the link from the route.
+    $link = Url::fromRoute('simple_affiliate.settracking');
+    $link = $link->toString();
+    $host = \Drupal::request()->getSchemeAndHttpHost();
+    return [
+      '#markup' => $host . $link,
+    ];
+  }
+
+}

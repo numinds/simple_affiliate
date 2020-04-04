5f2f22f0bfe5a14aa722207443cae017e32ca56c
Binny K Thomas <binny.thomas@axelerant.com>
2020-04-04 19:28:32 +0530

    Issue #3124965: Add uid parameter to affiliate cookie set url.

--- src/Controller/SetTracking.php
+++ src/Controller/SetTracking.php
@@ -14,7 +14,7 @@ class SetTracking extends ControllerBase {
   /**
    * Setting the tracking cookie.
    */
-  public function setaffilliatetrackingcookie($uid = 10) {
+  public function setaffilliatetrackingcookie($uid) {
     // Add affiliate cookie with 6 month expiration.
     if (!isset($_COOKIE["simple_affiliate"])) {
       setcookie("simple_affiliate", $uid, time() + 60 * 60 * 24 * 180, "/");
